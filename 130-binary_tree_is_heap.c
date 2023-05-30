67f9987572830a5d449c1e6eeaaa38f7037c49a1
Ohinoyi <ohimoiza12@gmail.com>
2023-05-30 16:44:28 +0100

    Create 130-binary_tree_is_heap.c

---
+++ 130-binary_tree_is_heap.c
@@ -0,0 +1,61 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_heap - checks if a binary tree is a valid Max Binary Heap
+ */
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (btih_helper(tree));
+}
+
+int btih_helper(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (1);
+
+	if (!binary_tree_is_complete(tree))
+		return (0);
+
+	if (tree->left)
+		if (tree->left->n > tree->n)
+			return (0);
+	if (tree->right)
+		if (tree->right->n > tree->n)
+			return (0);
+
+	return (btih_helper(tree->left) &&
+		btih_helper(tree->right));
+}
+
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (!tree)
+		return (0);
+	size = binary_tree_size(tree);
+
+	return (btic_helper(tree, 0, size));
+}
+
+int btic_helper(const binary_tree_t *tree, size_t index, size_t size)
+{
+	if (!tree)
+		return (1);
+
+	if (index >= size)
+		return (0);
+
+	return (btic_helper(tree->left, 2 * index + 1, size) &&
+		btic_helper(tree->right, 2 * index + 2, size));
+}
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) +
+		binary_tree_size(tree->right) + 1);
+}
