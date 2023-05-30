bae57b070400b1f6c6a269e7691049ab0a6bb052
Ohinoyi <ohimoiza12@gmail.com>
2023-05-30 15:57:20 +0100

    Create 113-bst_search.c

---
+++ 113-bst_search.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - searches for a value in a Binary Search Tree
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	bst_t *node = (bst_t *)tree;
+
+	if (!tree)
+		return (NULL);
+
+	while (node)
+	{
+		if (value == node->n)
+			return (node);
+		if (value < node->n)
+			node = node->left;
+		else if (value > node->n)
+			node = node->right;
+	}
+
+	return (NULL);
+}
