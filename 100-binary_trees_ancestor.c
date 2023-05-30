5d21c52965635f30b202503fd2c3c728144948ad
Ohinoyi <ohimoiza12@gmail.com>
2023-05-30 17:10:01 +0100

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,6 +2,8 @@
 
 /**
  * binary_tree_depth - measures the depth of a node in a binary tree.
+ *@tree: pointer to the node to measure the depth.
+ * Return: if tree is NULL, function must return 0.
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
@@ -10,7 +12,12 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	else
 		return (1 + binary_tree_depth(tree->parent));
 }
-
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: first node
+ * @second: second node
+ * Return: lowest common ancestor
+ */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
